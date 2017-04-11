7a56876b235c4c2261a46969e0653e6ec854133c
CarinaKr <carina.krafft@t-online.de>
2017-04-11 07:32:18 +0200

    unity project

---
+++ UFO/Assets/scripts/createAsteroids.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createAsteroids : MonoBehaviour {
+
+    public GameObject[] asteroids;
+    
+    public int asteroidsNumber;
+    public float minSize, maxSize;
+
+    private float sphereSize;
+    private float x, y, z,xRot,yRot,zRot;
+    private float size;
+    private GameObject asteroid;
+
+	// Use this for initialization
+	void Start () {
+
+        sphereSize = GetComponent<SphereCollider>().radius;
+
+        for (int i = 0; i < asteroidsNumber; i++)
+        {
+            x = Random.Range(-sphereSize , sphereSize )+transform.position.x;
+            y = Random.Range(-sphereSize , sphereSize )+transform.position.y;
+            z = Random.Range(-sphereSize , sphereSize )+transform.position.z;
+
+            xRot = Random.Range(0, 360);
+            yRot = Random.Range(0, 360);
+            zRot = Random.Range(0, 360);
+
+            size = Random.Range(minSize, maxSize);
+
+            asteroid = asteroids[Random.Range(0, asteroids.Length)];
+
+            Instantiate(asteroid, new Vector3(x, y,z), Quaternion.Euler(xRot,yRot,zRot));
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UFO/Assets/scripts/moveAsteroid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveAsteroid : MonoBehaviour {
+
+    private Vector3 zRichtung;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     void OnTriggerExit(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            Debug.Log("trigger exit");
+            zRichtung = other.transform.position - transform.position;
+            transform.Translate(zRichtung *2,Space.World);
+        }
+    }
+}
