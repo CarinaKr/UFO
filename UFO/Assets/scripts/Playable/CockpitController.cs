1cc83e80288cbca5df98b6dd10f610fc19d7da51
KevinHagen <kvn.hagen@gmail.com>
2017-05-14 21:39:17 +0200

    Import VR-Assets, Add Testing for Playable Version

---
+++ UFO/Assets/scripts/Playable/CockpitController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CockpitController : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0, moveZ);
+
+        rb.AddForce(movement);
+    }
+}
---
+++ UFO/Assets/scripts/Playable/TestSkript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+
+public class TestSkript : MonoBehaviour {
+
+    [SerializeField] private SelectionRadial m_SelectionRadial;
+    [SerializeField] private VRInteractiveItem m_InteractiveItem;
+
+    private bool m_GazeOver;
+
+    private void OnEnable()
+    {
+        m_InteractiveItem.OnOver += HandleOver;
+        m_InteractiveItem.OnOut += HandleOut;
+        m_SelectionRadial.OnSelectionComplete += HandleSelectionComplete;
+    }
+
+    private void OnDisable()
+    {
+        m_InteractiveItem.OnOver -= HandleOver;
+        m_InteractiveItem.OnOut -= HandleOut;
+        m_SelectionRadial.OnSelectionComplete -= HandleSelectionComplete;
+    }
+
+    private void HandleOver()
+    {
+        m_SelectionRadial.Show();
+
+        m_GazeOver = true;
+    }
+
+    private void HandleOut()
+    {
+        m_SelectionRadial.Hide();
+
+        m_GazeOver = false;
+    }
+
+    private void HandleSelectionComplete()
+    {
+        if (m_GazeOver)
+            Debug.Log("Action Complete");
+    }
+}
