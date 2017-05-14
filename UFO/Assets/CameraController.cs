d96fbb9b5ce8507f5d2e36dfdeffcdc2a73d10a6
KevinHagen <kvn.hagen@gmail.com>
2017-05-14 23:48:06 +0200

    Create CameraController, Fixed Issue with Interactive Item

---
+++ UFO/Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = player.position - gameObject.transform.position;	
+	}
+
+	void LateUpdate () {
+        gameObject.transform.position = player.position - offset;
+	}
+}
