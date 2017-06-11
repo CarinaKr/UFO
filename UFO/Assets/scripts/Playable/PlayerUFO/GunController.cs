eae76f9549cafd9fc5339920f92eba6b1fcef4b7
KevinHagen <kvn.hagen@gmail.com>
2017-06-11 23:43:08 +0200

    Completed Strategy-Pattern for Guns
    I have completed working on an implementation of the strategy pattern
    for the Guns. It seems to work, but has to be tested further (especially
    with more Guns implemented).

--- UFO/Assets/scripts/Playable/PlayerUFO/GunController.cs
+++ UFO/Assets/scripts/Playable/PlayerUFO/GunController.cs
@@ -5,26 +5,38 @@ using UnityEngine;
 public class GunController : MonoBehaviour {
 
     private Gun gun;
+    private int nextWeaponIndex;
 
-	// Use this for initialization
-	void Start () {
+    public Transform[] barrelEnds;
+    private IGunStrategy[] strategies = new IGunStrategy[2];
+
+    // Use this for initialization
+    void Start () {
+        fillStrategies();
+
         gun = new Gun();
-        gun.setGunStrat(GetComponentInChildren<RailGun>());
+        gun.setGunStrat(strategies[nextWeaponIndex], barrelEnds[nextWeaponIndex]);
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Fire2"))
+		if(Input.GetButtonDown("Jump"))
         {
-            Debug.Log("Pressed");
-            IGunStrategy strategy = GetComponentInChildren<RocketLauncher>();
-            Transform barrelEnd = GetComponentInChildren<RocketLauncher>(); //get the transform of correct barrelEnd here and pass it into setGunStrat
-            gun.setGunStrat(strategy);
+            nextWeaponIndex++;
+            nextWeaponIndex %= strategies.Length;
+            gun.setGunStrat(strategies[nextWeaponIndex], this.barrelEnds[nextWeaponIndex]);
         }
 	}
 
     void FixedUpdate()
     {
         gun.Shoot();
+    }
+
+    void fillStrategies()
+    {
+        strategies[0] = GetComponentInChildren<RailGun>();
+        strategies[1] = GetComponentInChildren<RocketLauncher>();
     }
 }
