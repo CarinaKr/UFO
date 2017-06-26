205e57fd40bffe6bfe62a597c35675b41df9f55c
CarinaKr <carina.krafft@t-online.de>
2017-06-26 22:34:26 +0200

    possible solution for asteroid movement

--- UFO/Assets/scripts/Playable/Asteroids/createAsteroids.cs
+++ UFO/Assets/scripts/Playable/Asteroids/createAsteroids.cs
@@ -6,7 +6,8 @@ public class createAsteroids : MonoBehaviour {
 
     public GameObject[] asteroids;
     public Transform ufo;
-    
+
+    public int circlesNumber;
     public int asteroidsNumber;
     public float minSize, maxSize;
     public float radius;
@@ -20,27 +21,34 @@ public class createAsteroids : MonoBehaviour {
 	void Start () {
 
         sphereSize = radius;
-
-        for (int i = 0; i < asteroidsNumber; i++)
+        for(int j=0;j<circlesNumber;j++)
         {
-            x = Random.Range(-sphereSize, sphereSize);
-            y = Random.Range(0, sphereSize / 2);
-            z = Random.Range(-sphereSize, sphereSize);
+               for (int i = 0; i < asteroidsNumber; i++)
+                {
+                    x = Random.Range(-sphereSize, sphereSize);
+                    y = Random.Range(0, sphereSize / 4);
+                    z = Random.Range(-sphereSize, sphereSize);
 
-            xRot = Random.Range(0, 360);
-            yRot = Random.Range(0, 360);
-            zRot = Random.Range(0, 360);
+                    xRot = Random.Range(0, 360);
+                    yRot = Random.Range(0, 360);
+                    zRot = Random.Range(0, 360);
 
-            size = Random.Range(minSize, maxSize);
+                    size = Random.Range(minSize, maxSize);
 
-            asteroid = asteroids[Random.Range(0, asteroids.Length)];
+                    asteroid = asteroids[Random.Range(0, asteroids.Length)];
 
-            if (asteroid != null)
-            {
-                asteroid.transform.localScale = new Vector3(size, size, size);
-                GameObject.Instantiate(asteroid, new Vector3(x, y, z), Quaternion.Euler(xRot, yRot, zRot), gameObject.transform);
-            }
+                    if (asteroid != null)
+                    {                                               //Ring aus Asteroiden am Rand des Radius:
+                        Vector3 direction = new Vector3(x, 0,z);    //Richtung in xz-Ebene
+                        direction.Normalize();                      //Normalenvektor der Richtung
+                        direction *= radius-(j*(radius/circlesNumber));                        //und mit dem Radius multiplizieren
+                        direction.y = y;                           //zufällige Höhe setzen
+                        asteroid.transform.localScale = new Vector3(size, size, size);
+                        GameObject.Instantiate(asteroid, direction, Quaternion.Euler(xRot, yRot, zRot), gameObject.transform);
+                    }
+                }
         }
+       
 
 	}
 	
