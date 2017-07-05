23c800abc27b4617cccdeea18ed2e6a285f2d7b9
KevinHagen <kvn.hagen@gmail.com>
2017-07-05 18:24:07 +0200

    Add Testscene for Sascha

---
+++ UFO/Assets/scripts/Test/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+
+    public float moveSpeed = 1f;
+    public Transform center;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0, moveZ) * moveSpeed * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            transform.RotateAround(center.position, Vector3.up, 15f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.RotateAround(center.position, Vector3.up, -15f);
+        }
+
+
+        transform.Translate(movement);
+
+
+    }
+}
